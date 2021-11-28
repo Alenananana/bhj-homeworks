48b1ef55134cfb4153e4e73d469b61e1f216d548
«AlenaKryukova» <asche91@mail.ru>
2021-11-28 16:39:06 +0500

    2

---
+++ element-search/menu/task.js
@@ -0,0 +1,24 @@
+let action = document.querySelectorAll('.menu__link');
+
+for (let i of action) {
+
+    let parent = i.closest('.menu__item');
+
+    i.addEventListener('click', function abc(event) {
+
+        if (parent.querySelector('.menu_sub') && parent.querySelector('.menu_active')) {
+            parent.querySelector('.menu_active').classList.remove('menu_active');
+            event.preventDefault();
+        } else if (parent.querySelector('.menu_sub')) {
+
+            if (document.querySelector('.menu_active')) {
+                document.querySelector('.menu_active').classList.remove('menu_active');
+                event.preventDefault();
+            }
+            parent.querySelector('.menu_sub').classList.add('menu_active');
+            event.preventDefault();
+        }
+    })
+}
+
+menu();
