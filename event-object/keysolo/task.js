c7563831f5dc0ed1af96167efe6d99c86c8fd00d
«AlenaKryukova» <asche91@mail.ru>
2021-11-29 19:52:27 +0500

    2

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,14 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+      document.addEventListener('keypress', (event) => {
+          const keyName = event.key;
+          if (keyName.toLowerCase() === this.currentSymbol.textContent.toLowerCase()) {
+              this.success();
+          } else {
+              this.fail();
+          }
+      });
   }
 
   success() {
